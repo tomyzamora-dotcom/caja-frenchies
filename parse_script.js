eff3d2ea93e97494e1032bf1bb1e9cd75b00a9b1
tomyzamora-dotcom <tomyzamora@gmail.com>
2026-06-06 23:25:57 -0300

    Add files via upload

---
+++ parse_script.js
@@ -0,0 +1,10 @@
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var file = fso.OpenTextFile("script.js", 1, false, -1);
+var code = file.ReadAll();
+file.Close();
+try {
+  new Function(code);
+  WScript.Echo("OK");
+} catch(e) {
+  WScript.Echo("ERR:" + e.message);
+}
